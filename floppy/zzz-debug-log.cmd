74f3c096b8dab65b202583e09600b798b0f818a5
Ross Smith II <ross@smithii.com>
2014-05-27 14:02:36 -0700

    fix typo in PACKER_LOG_DIR

--- floppy/zzz-debug-log.cmd
+++ floppy/zzz-debug-log.cmd
@@ -12,7 +12,7 @@ type "%PACKER_DEBUG_LOG%"
 
 if not defined PACKER_LOG_DIR set PACKER_LOG_DIR=z:\c\packer_logs
 
-set PACKER_LOG_PATH=%PACKER_LOG_DIR %\%COMPUTERNAME%
+set PACKER_LOG_PATH=%PACKER_LOG_DIR%\%COMPUTERNAME%
 if not exist "%PACKER_LOG_PATH%" mkdir "%PACKER_LOG_PATH%"
 if not exist "%PACKER_LOG_PATH%" echo ==^> ERROR: Unable to create directory "%PACKER_LOG_PATH%" & goto exit1
 
